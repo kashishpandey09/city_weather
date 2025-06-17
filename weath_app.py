b8859efba57bdc28569a0730edda2ac526655dd4
kashishpandey09 <pkashish852@gmail.com>
2025-06-17 09:36:28 +0530

    Update weath_app.py

--- weath_app.py
+++ weath_app.py
@@ -11,17 +11,17 @@ st.set_page_config(layout="wide")
 
 # ✅ HIDE STREAMLIT MENU, FOOTER, ETC.
  hide_streamlit_cloud_elements = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    header {visibility: hidden;}
-    footer {visibility: hidden;}
-    .stDeployButton {display: none;}
-    a[title="View source"] {display: none !important;}
-    button[kind="icon"] {display: none !important;}
-    </style>
+<style>
+#MainMenu {visibility: hidden;}
+header {visibility: hidden;}
+footer {visibility: hidden;}
+.stDeployButton {display: none;}
+a[title="View source"] {display: none !important;}
+button[kind="icon"] {display: none !important;}
+</style>
 """
-st.markdown(hide_streamlit_cloud_elements, unsafe_allow_html=True)
 
+st.markdown(hide_streamlit_cloud_elements, unsafe_allow_html=True)
 # Function to convert Unix timestamp to local time
 def unix_to_local_time(unix_timestamp, timezone_offset):
     local_time = datetime.utcfromtimestamp(unix_timestamp) + timedelta(seconds=timezone_offset)
